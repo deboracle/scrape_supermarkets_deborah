4182bdfb94d2feb7a72a755b9d23bc2aec85005f
deboracle <deborah.gironde@gmail.com>
2023-09-05 14:26:48 +0300

    deleted the unuseful part that took all barcodes from victory, changed the filtering part, and some other stuff

--- main.py
+++ main.py
@@ -2,25 +2,10 @@ import xml.etree.ElementTree as ET
 import sqlite3
 import requests
 
+# TODO: yohnanof, hazi heinam, maayan 2000, carrefour
 # URLs needed
-urls = {'mck': 'https://www.mck.co.il/', 'victory': 'https://www.victoryonline.co.il'}
-
-
-def get_all_from_victory():
-    # Get all barcodes from victory price file
-    tree = ET.parse("theprices.xml")
-    root = tree.getroot()
-
-    # Extract barcode information
-    victory_barcodes = []
-
-    for product_elem in root.findall(".//Product"):
-        barcode_elem = product_elem.find("ItemCode")
-        if barcode_elem is not None:
-            barcode = barcode_elem.text
-            victory_barcodes.append(barcode)
-    print(f"Extracted { len(victory_barcodes) } barcodes from victory")
-    return victory_barcodes
+# https://www.mck.co.il/v2/retailers/1470/branches/2331/products?filters=%7B%22must%22:%7B%22term%22:%7B%22branch.isVisible%22:true%7D%7D%7D&from=1&size=500
+urls = {'mck': 'https://www.mck.co.il', 'victory': 'https://www.victoryonline.co.il'}
 
 
 def get_all():
@@ -36,10 +21,9 @@ def get_all():
 
 def scrape(supermarket):
     # Define url and params, branch will always be 2930 for internet
-    # TODO: params not working for mck
     url = f'{ supermarket }/v2/retailers/1470/branches/2930/products'
     params = {
-        'filters': '{"must":{"term":{"branch.isVisible":false}}}',
+        'filters': '{"must":{"term":{"branch.isVisible":true}}}',
         'from': 1,
         'size': 500
     }
@@ -72,13 +56,15 @@ def insert_to_db(raw_data):
 
 
 def main(all_products):
+    print(len(all_products))
     # Convert the other supermarket barcodes list to a set for faster membership checking
     other_supermarket_set = set(get_all())
-    filtered_barcodes = [barcode for barcode in get_all_from_victory() if barcode not in other_supermarket_set]
-    filtered_products = [product for product in all_products if product.get("barcode") in filtered_barcodes]
-    print(filtered_products)
+    # filter products and keep only the ones that don't appear in the sqlite file.
+    filtered_products = [product for product in all_products if product.get("barcode") not in other_supermarket_set]
+    print(filtered_products, len(filtered_products))
     return filtered_products
 
 
 if __name__ == "__main__":
     main(all_products=scrape(urls[input("Please enter the name of the supermarket: ")]))
+    # TODO: convert and insert to table

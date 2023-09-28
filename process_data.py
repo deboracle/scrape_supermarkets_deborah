c2f6c58ca4d2a85c042d6734f7e60c646033d9ae
deboracle <deborah.gironde@gmail.com>
2023-09-28 19:19:53 +0300

    checking if key exists at all

--- process_data.py
+++ process_data.py
@@ -1,10 +1,21 @@
 # This code processes the raw data from the json files and inserts them to a table
-# What we want to insert:
-# description, nutrition itm (ingredients), symbole (sucar, shuman), unite de mesure, categorie, pct de fruits,
-# marque, allergenes, may contain, image url
+# TODO: check if key exists, if not return null
 
 import sqlite3
 import json
+
+
+def check_key(data, keys):
+    # This function returns None if the key doesnt exist
+    current_key = keys[0]
+
+    if current_key in data:
+        if len(keys) == 1:
+            return data[current_key]
+        else:
+            return check_key(data[current_key], keys[1:])
+    else:
+        return None
 
 
 def process_json_data(json_file_path):
@@ -14,36 +25,32 @@ def process_json_data(json_file_path):
         data = json.load(json_file)
 
         # This helper function will return None if a key is not found in the JSON
-        def get_key_or_none(dictionary, key):
-            return dictionary.get(key, None)
 
+        # TODO: try to find symbol, fruit pct
         for item in data:
-            try:
-                product_data = {}
-                product_data['barcode'] = item['barcode']
-                product_data['ingredients'] = item['data']['1']['ingredients']
-                product_data['unit_of_measure'] = get_key_or_none(item['UnitOfMeasure']['defaultName']) #we can change it to id
-                product_data['brand'] = get_key_or_none(item['brand']['names']['2'])
-                product_data['allergens'] = get_key_or_none(item['data']['1']['containAllergens'])
-                product_data['image_url'] = item['image']['url']
+            product_data = {}
+            product_data['barcode'] = check_key(item, ['barcode'])
+            product_data['ingredients'] = check_key(item, ['data', '1', 'ingredients'])
+            product_data['unit_of_measure'] = check_key(item, ['unitOfMeasure', 'defaultName']) #we can change it to id
+            product_data['brand'] = check_key(item, ['brand', 'names', '2'])
+            product_data['allergens'] = check_key(item, ['data', '1', 'containAllergens'])
+            product_data['might_contain'] = check_key(item, ['data', '1', 'mightContainAllergens'])
+            product_data['image_url'] = check_key(item, ['image', 'url'])
+            product_data['name_he'] = check_key(item, ['names', '1', 'short'])
+            product_data['name_en'] = check_key(item, ['names', '2', 'short'])
 
-                # Extract all names for product
-                names = []
-                for name in item['names']:
-                    names.append(name['short'])
+            print(product_data)
 
-                # Extract all categories for product
-                categories_list = []
-                for categories in item['family']['categories']:
-                    categories_list.append([categories['names']['2'], categories['names']['1']])
+            # Extract all categories for product
+            categories_list = []
+            for categories in item['family']['categories']:
+                categories_list.append([categories['names']['2'], categories['names']['1']])
 
-                # same for nutrition values
-                nutr_values = item['nutritionValues']
+            # same for nutrition values
+            nutr_values = item['nutritionValues']
 
-                data_list.append(product_data)
-
-            except KeyError:
-                print('Key does not exist for this item')
+            data_list.append(product_data)
+            print(product_data)
 
     return data_list
 
@@ -53,27 +60,28 @@ def insert_to_sqlite(data_list, db_file):
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
 
+    # TODO: Nutrition values, Fruit Pct.
     # Create a table if it doesn't exist
     cursor.execute('''CREATE TABLE IF NOT EXISTS products (
-                        description TEXT,
-                        ingredients TEXT,
-                        symbol TEXT,
-                        unit_of_measure TEXT,
-                        categorie TEXT,
-                        pct_de_fruits REAL,
-                        marque TEXT,
-                        allergenes TEXT,
-                        may_contain TEXT,
-                        image_url TEXT
+                        Description TEXT NOT NULL,
+                        ItemCode int NOT NULL,
+                        Ingredients TEXT,
+                        UnitOfMeasure TEXT,
+                        Categories TEXT,
+                        Brand TEXT,
+                        Allergens TEXT,
+                        MightContain TEXT,
+                        ImageUrl TEXT,
+                        PRIMARY KEY(ItemCode)
                     )''')
 
     # Insert data into the table
     for product_data in data_list:
-        cursor.execute('''INSERT INTO products (description, ingredients, symbol, unit_of_measure, categorie, 
-                        pct_de_fruits, marque, allergenes, may_contain, image_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                       (product_data['description'], product_data['ingredients'], product_data['symbol'],
-                        product_data['unit_of_measure'], product_data['categorie'], product_data['pct_de_fruits'],
-                        product_data['marque'], product_data['allergenes'], product_data['may_contain'], product_data['image_url']))
+        cursor.execute('''INSERT INTO products (description, ItemCode, Ingredients, UnitOfMeasure, Category, 
+                        Brand, Allergens, MightContain, ImageUrl) VALUES (?, ?, ?, ?, ?, ?, ?)''',
+                       (product_data['description'], product_data['barcode'], product_data['ingredients'],
+                        product_data['unit_of_measure'], product_data['categories'], product_data['brand'],
+                        product_data['allergens'], product_data['might_contain'], product_data['image_url']))
 
     conn.commit()
     conn.close()
@@ -81,7 +89,7 @@ def insert_to_sqlite(data_list, db_file):
 
 if __name__ == "__main__":
     json_file_path = './mck_data.json'
-    db_file = 'your_database.db'  # Replace with your SQLite database file path
+    db_file = '/Users/deborahgironde/Downloads/deborah_data.db'  # Replace with your SQLite database file path
 
     data_list = process_json_data(json_file_path)
     insert_to_sqlite(data_list, db_file)
